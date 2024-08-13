f60f43d77dd870e726ea67180f59e8624e8c85d7
sazypapi <128397579+sazypapi@users.noreply.github.com>
2024-08-13 15:01:23 +0100

    contact

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
   return (
     <>
       <div className="contact-us">
-        <h1>Communication with you is our priority !</h1>
+        <h1>Communication with you is our priority</h1>
         <h4>
           Effective communication with our members at Soldout Christian Center
           is essential because it fosters a strong sense of community and
@@ -19,33 +19,35 @@ const ContactUs = () => {
           mission to impact lives and bring people closer to God.
         </h4>
       </div>
-      <h3>Below are ways you can contact us!</h3>
-      <a href="tel:08083975406">
-        <div className="contact-card">
-          <img src={phone} alt="" />
-          <h4>08083975406</h4>
-        </div>
-      </a>
-      <a href="tel:08141998600">
-        <div className="contact-card">
-          <img src={phone} alt="" />
-          <h4>08141998600</h4>
-        </div>
-      </a>
+      <h3 className="contact-h3">Below are ways you can contact us!</h3>
+      <div className="ccc">
+        <a href="tel:08083975406">
+          <div className="contact-card">
+            <img src={phone} alt="" />
+            <h4>08083975406</h4>
+          </div>
+        </a>
 
-      <a href="mailto:soldoutchristiancenter2015@gmail.com">
-        <div className="contact-card">
-          <img src={email} alt="" />
-          <h4>soldoutchristiancenter2015@gmail.com</h4>
-        </div>
-      </a>
+        <a href="mailto:soldoutchristiancenter2015@gmail.com">
+          <div className="contact-card">
+            <img src={email} alt="" />
+            <h4>soldoutchristiancenter2015@gmail.com</h4>
+          </div>
+        </a>
+        <a href="tel:08141998600">
+          <div className="contact-card">
+            <img src={phone} alt="" />
+            <h4>08141998600</h4>
+          </div>
+        </a>
 
-      <a href="https://www.instagram.com/soldoutchristiancenter?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-        <div className="contact-card">
-          <img src={instagram} alt="" />
-          <h4>@soldoutchristiancenter</h4>
-        </div>
-      </a>
+        <a href="https://www.instagram.com/soldoutchristiancenter?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+          <div className="contact-card">
+            <img src={instagram} alt="" />
+            <h4>soldoutchristiancenter</h4>
+          </div>
+        </a>
+      </div>
       <Footer />
     </>
   );
