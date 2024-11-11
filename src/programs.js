4c009b6d8365883053f4d24eb693d1a3278e865f
sazypapi <128397579+sazypapi@users.noreply.github.com>
2024-11-11 16:50:21 +0100

    ANNUAL THANKSGIVING

--- src/programs.js
+++ src/programs.js
@@ -1,18 +1,14 @@
-import campMeeting from "./assets/Camp Meeting web.png";
-import campSquare from "./assets/camp square.png";
-import praiseQuakeSquare from "./assets/praise quake square.png";
-import praiseQuake from "./assets/praise quake web.png";
-
+import annualthanksgiving from "./assets/annualthanksgiving.jpg";
+import annualthanksgivinglarge from "./assets/annualthanksgivinglarge.png";
 const programs = [
   {
     id: 1,
-    name: "Praise Quake",
-    date: "October 5th, 2024",
-    description:
-      "Praise Quake is our annual praise program that has had ministers like Frank Edwards, Victoria Orenze,Buchi, Preye Odede, Uche Unlimited and many more grace her stage. The program's main aim is to capture souls for God through truthful and soulful worship. This year we have artists like Pastor Lawrence Oyor, Buchi, Steve Hills, Uche Unlimited and many more gracing our stage. DON'T MISS IT!",
-    img: praiseQuake,
-    location: "Mater dei schools, 109 Marwa Road, Satellite town, Lagos",
-    imgSquare: praiseQuakeSquare,
+    name: "Annual Thanksgiving",
+    date: "November 17th, 2024",
+    description: "Come and thank God for all he's done in the past year",
+    img: annualthanksgivinglarge,
+    location: "Church Auditorium",
+    imgSquare: annualthanksgiving,
   },
 ];
 export default programs;
