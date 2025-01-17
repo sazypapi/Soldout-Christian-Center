7da25aec33f7c79fda277a5eed9c4bf8dd504285
sazypapi <128397579+sazypapi@users.noreply.github.com>
2025-01-17 09:46:09 +0100

    35 Days Edit

--- src/programs.js
+++ src/programs.js
@@ -1,14 +1,15 @@
-import annualthanksgiving from "./assets/annualthanksgiving.jpg";
-import annualthanksgivinglarge from "./assets/annualthanksgivinglarge.png";
+import t5 from "./assets/35 days.jpg";
+import tflarge from "./assets/35 days large.jpg";
 const programs = [
   {
     id: 1,
-    name: "Annual Thanksgiving",
-    date: "November 17th, 2024",
-    description: "Come and thank God for all he's done in the past year",
-    img: annualthanksgivinglarge,
+    name: "35 DAYS NIGHT VIGIL",
+    date: "5th January - 8th February 2025",
+    description:
+      "It's that time of the year again! Our annual 30-day night vigil is here, but this year is special—it's 35 days! The theme is 'Overflowing Mercy, Grace, and Blessings.' Join us every day at 10 PM as we commit our year into the hands of the Lord.",
+    img: tflarge,
     location: "Church Auditorium",
-    imgSquare: annualthanksgiving,
+    imgSquare: t5,
   },
 ];
 export default programs;
